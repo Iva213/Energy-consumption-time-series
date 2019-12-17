3b419cd293ef9026b8f4adbf54e05559951798d9
Iva Tzvetkova <Iva@Ivas-MacBook-Air.local>
2019-12-17 10:57:58 +0100

    Removing NAs

--- NA.imputation.R
+++ NA.imputation.R
@@ -34,7 +34,7 @@ energy.merged$Hour <- hour(energy.merged$DateTime)
 energy.merged$Minute <- minute(energy.merged$DateTime)
 
 #Re-order columns
-energy.merged <- energy.merged[, c(1, 10, 9, 8, 7, 6, 5, 2:4)]
+energy.merged <- energy.merged[, c(1, 11, 10, 9, 8, 7, 6, 2:5)]
 
 #I have no idea how to remove the 180 NAs which arise from time changes 
 #So, I used na.omit()
@@ -43,7 +43,7 @@ for (i in 1:seq_along(energy.merged)) {
     energy.merged[[i]][is.na(reg.timeser[[i]])] <- energy.merged[[i]]
   }
 
-energy.merged <- na.omit(energy.merged)
+
 
 energy.merged$Day <- factor(energy.merged$Day, levels = c("Mon", "Tue", "Wed",
                                             "Thu", "Fri", "Sat",
@@ -52,37 +52,12 @@ energy.merged$Day <- factor(energy.merged$Day, levels = c("Mon", "Tue", "Wed",
 ################################################
 
 #Impute missing values for the 3 sub-meters 
-energy.merged$Sub_metering_1 <- na_interpolation(energy.merged$Sub_metering_1, option = "spline")
-energy.merged$Sub_metering_2 <- na_interpolation(energy.merged$Sub_metering_2, option = "spline")
-energy.merged$Sub_metering_3 <- na_interpolation(energy.merged$Sub_metering_3, option = "spline")
+energy.merged$Sub_metering_1 <- na_kalman(energy.merged$Sub_metering_1)
+energy.merged$Sub_metering_2 <- na_kalman(energy.merged$Sub_metering_2)
+energy.merged$Sub_metering_3 <- na_kalman(energy.merged$Sub_metering_3)
+energy.merged$Global_active_power <- na_kalman(energy.merged$Global_active_power)
 
-################################################
+energy.merged <- na.omit(energy.merged) #Get rid of 180 NAs
 
-## Weekly avg sub-meter reading 
 
-weekly.meter <- weekly.grouping(energy.merged)
-energy.merged.weekly.sub1 <- data.frame(weekly.meter$sub1)
-energy.merged.weekly.sub2 <- data.frame(weekly.meter$sub2)
-energy.merged.weekly.sub3 <- data.frame(weekly.meter$sub3)
-
-## Monthly avg sub-meter reading 
-
-monthly.meter <- monthly.grouping(energy.merged)
-energy.merged.monthly.sub1 <- data.frame(monthly.meter$sub1)
-energy.merged.monthly.sub2 <- data.frame(monthly.meter$sub2)
-energy.merged.monthly.sub3 <- data.frame(monthly.meter$sub3)
-
-################################################
-
-## Time series analysis 
-
-#Weekly
-sub1.ts <- time.series.analysis(energy.merged.weekly.sub1$avg.sub1)
-sub2.ts <- time.series.analysis(energy.merged.weekly.sub2$avg.sub2)
-sub3.ts <- time.series.analysis(energy.merged.weekly.sub3$avg.sub3)
-
-#Monthly
-sub1.ts.monthly <- time.series.analysis(energy.merged.monthly.sub1$avg.sub1)
-sub2.ts.monthly <- time.series.analysis(energy.merged.monthly.sub1$avg.sub1)
-sub3.ts.monthly <- time.series.analysis(energy.merged.monthly.sub3$avg.sub3)
 
